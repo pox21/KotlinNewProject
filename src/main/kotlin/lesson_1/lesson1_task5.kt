ce5026dc42910129f3571259274fe0ca459e0074
Web-Entwickler <pixpox21@bk.ru>
2025-09-28 16:11:41 +0200

    KS-1-5-variables clear comments task 5

--- src/main/kotlin/lesson_1/lesson1_task5.kt
+++ src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -2,16 +2,10 @@ package lesson_1
 const val MINUTES_IN_HOUR = 60
 const val SECONDS_IN_HOUR = MINUTES_IN_HOUR * MINUTES_IN_HOUR
 fun main() {
-//    Объяви переменную с количеством секунд, которые Гагарин провел в космосе.
-//
-//    - Переведи в коде количество секунд в целые минуты и целые часы,
-//    сохраняя остаток секунд и минут в новые переменные;
-//    - Выведи в консоль время, проведенное в космосе в формате [часы:минуты:секунды],
-//    например так: 01:30:09;
-//    - Значения часов, минут и секунд отображать двумя цифрами.
+
     val totalSeconds: Int = 6480
     val secondsInHour = MINUTES_IN_HOUR * MINUTES_IN_HOUR
-    // Перевод в часы, минуты и секунды
+
     val hours: Int = totalSeconds / secondsInHour
     val remainingSeconds: Int = totalSeconds % secondsInHour
 
