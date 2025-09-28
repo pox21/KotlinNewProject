e1edc3e3f8b8e1a59e941b47c410bf69389e8f1c
Web-Entwickler <pixpox21@bk.ru>
2025-09-28 15:14:15 +0200

    KS-1-3-variables fixed task 3

--- src/main/kotlin/lesson_1/lesson1_task3.kt
+++ src/main/kotlin/lesson_1/lesson1_task3.kt
@@ -7,9 +7,9 @@ fun main() {
     var launchHour = 9
     var launchMinute = 7
 
-    println("Юрий Гагарин полетел в космос в $yearFlight году")
-    println("часы: %02d".format(launchHour))
-    println("минуты: %02d".format(launchMinute))
+    println("Юрий Гагарин полетел в космос в $yearFlight году");
+    println("часы: 0$launchHour");
+    println("минуты: 0$launchMinute")
 
     launchHour = 10
     launchMinute = 55
