1964cdc57ca4c326d711d2ca89f0bcdfe7aa0d71
Web-Entwickler <pixpox21@bk.ru>
2025-09-28 16:28:36 +0200

    KS-1-3-variables fixed comments task 3

--- src/main/kotlin/lesson_1/lesson1_task3.kt
+++ src/main/kotlin/lesson_1/lesson1_task3.kt
@@ -8,11 +8,11 @@ fun main() {
     var launchMinute = 7
 
     println("Юрий Гагарин полетел в космос в $yearFlight году");
-    println("часы: 0$launchHour");
-    println("минуты: 0$launchMinute");
+    println("часы: %02d".format(launchHour))
+    println("минуты: %02d".format(launchMinute))
 
     launchHour = 10;
     launchMinute = 55;
-    print("$launchHour:$launchMinute");
+    print("%02d:%02d".format(launchHour, launchMinute))
 
 }
