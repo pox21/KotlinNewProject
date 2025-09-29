840d8d1ed29957b37de7bd1205b74c67c1eaad2e
Web-Entwickler <pixpox21@bk.ru>
2025-09-29 21:00:32 +0200

    KS-3-1 Task 1

---
+++ src/main/kotlin/lesson_3/lesson_3_task1.kt
@@ -0,0 +1,10 @@
+package lesson_3
+
+fun main() {
+    val userName: String = "Юрий"
+    var greeting: String = "Добрый день"
+    println("$greeting, $userName!")
+
+    greeting = "Добрый вечер"
+    println("$greeting, $userName!")
+}
