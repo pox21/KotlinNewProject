31f0fb7b508aa60149522d363711da3be3945410
Web-Entwickler <pixpox21@bk.ru>
2025-09-29 11:38:15 +0200

    KS-2-1 removed empty lines Task 3

--- src/main/kotlin/lesson_2/lesson_2_task3.kt
+++ src/main/kotlin/lesson_2/lesson_2_task3.kt
@@ -1,38 +1,21 @@
 package lesson_2
-
+const val MINUTES_IN_HOUR: Int = 60
 fun main() {
-//    Сайт с расписанием поездов получает данные с сервера.
-//    Сервер посылает время выезда и время в пути, а время прибытия вычисляется из них.
-//    Сервер прислал данные, что поезд выехал в 9:39 и будет в пути 457 минут.
-//
-//    – Создай целочисленные переменные и проинициализируй их этими данными;
-//    – Напиши программу, которая обработает данные и подсчитает час и минуту прибытия поезда;
-//    – Выведи результат в консоль.
-
-//      Время выезда
     val departureHour: Int = 9
     val departureMinute: Int = 39
     println("Время выезда: %02d:%02d".format(departureHour, departureMinute))
-//      Время в пути (общее количество минут, в часы и минуты)
-    val travelTimeTotalMinutes: Int = 457
-    val travelTimeHours: Int = travelTimeTotalMinutes / 60
-    val travelTimeMinutes: Int = travelTimeTotalMinutes % 60
-//      Решение 1
-//    val arrivalHour: Int = departureHour + (travelTimeHours + (departureMinute + travelTimeMinutes) / 60)
-//    val arrivalMinute: Int = (departureMinute + travelTimeMinutes) % 60
 
+    val travelTimeTotalMinutes: Int = 457
+    val travelTimeHours: Int = travelTimeTotalMinutes / MINUTES_IN_HOUR
+    val travelTimeMinutes: Int = travelTimeTotalMinutes % MINUTES_IN_HOUR
+//      Решение 1
+//    val arrivalHour: Int = departureHour + (travelTimeHours + (departureMinute + travelTimeMinutes) / MINUTES_IN_HOUR)
+//    val arrivalMinute: Int = (departureMinute + travelTimeMinutes) % MINUTES_IN_HOUR
     println("Время в пути $travelTimeHours:$travelTimeMinutes")
 //    println("Время прибытия $arrivalHour:$arrivalMinute")
-
 //      Решение 2
-//      Общее количество минут с начала суток до прибытия
-    val totalArrivalMinutes: Int = departureHour * 60 + departureMinute + travelTimeTotalMinutes
-
-//      Часы и минуты прибытия
-    val arrivalHour: Int = (totalArrivalMinutes / 60) % 24
-    val arrivalMinute: Int = totalArrivalMinutes % 60
-
-//      Вывод
+    val totalArrivalMinutes: Int = departureHour * MINUTES_IN_HOUR + departureMinute + travelTimeTotalMinutes
+    val arrivalHour: Int = (totalArrivalMinutes / MINUTES_IN_HOUR) % 24
+    val arrivalMinute: Int = totalArrivalMinutes % MINUTES_IN_HOUR
     println("Время прибытия: $arrivalHour:$arrivalMinute")
-
 }
