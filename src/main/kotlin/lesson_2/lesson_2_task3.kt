0c097286ce3e96a4fe25c34723859346bdb71efb
Web-Entwickler <pixpox21@bk.ru>
2025-09-29 19:34:30 +0200

    KS-3-2 refactor  Task 3

--- src/main/kotlin/lesson_2/lesson_2_task3.kt
+++ src/main/kotlin/lesson_2/lesson_2_task3.kt
@@ -1,5 +1,7 @@
 package lesson_2
+
 const val MINUTES_IN_HOUR: Int = 60
+
 fun main() {
     val departureHour: Int = 9
     val departureMinute: Int = 39
@@ -8,14 +10,9 @@ fun main() {
     val travelTimeTotalMinutes: Int = 457
     val travelTimeHours: Int = travelTimeTotalMinutes / MINUTES_IN_HOUR
     val travelTimeMinutes: Int = travelTimeTotalMinutes % MINUTES_IN_HOUR
-//      Решение 1
-//    val arrivalHour: Int = departureHour + (travelTimeHours + (departureMinute + travelTimeMinutes) / MINUTES_IN_HOUR)
-//    val arrivalMinute: Int = (departureMinute + travelTimeMinutes) % MINUTES_IN_HOUR
-    println("Время в пути $travelTimeHours:$travelTimeMinutes")
-//    println("Время прибытия $arrivalHour:$arrivalMinute")
-//      Решение 2
-    val totalArrivalMinutes: Int = departureHour * MINUTES_IN_HOUR + departureMinute + travelTimeTotalMinutes
-    val arrivalHour: Int = (totalArrivalMinutes / MINUTES_IN_HOUR) % 24
-    val arrivalMinute: Int = totalArrivalMinutes % MINUTES_IN_HOUR
-    println("Время прибытия: $arrivalHour:$arrivalMinute")
+
+    val arrivalHour: Int = departureHour + (travelTimeHours + (departureMinute + travelTimeMinutes) / MINUTES_IN_HOUR)
+    val arrivalMinute: Int = (departureMinute + travelTimeMinutes) % MINUTES_IN_HOUR
+    println("Время в пути %02d:%02d".format(travelTimeHours, travelTimeMinutes))
+    println("Время прибытия %02d:%02d".format(arrivalHour, arrivalMinute))
 }
