d4298367dc88824c9631f70157d66a9a80bf7e4c
Web-Entwickler <pixpox21@bk.ru>
2025-09-29 20:41:09 +0200

    KS-5-2 refactor Task 5

--- src/main/kotlin/lesson_2/lesson_2_task5.kt
+++ src/main/kotlin/lesson_2/lesson_2_task5.kt
@@ -1,15 +1,12 @@
 package lesson_2
 
+import kotlin.math.pow
+
 fun main() {
     val initialDeposit: Double = 70_000.0
     val annualInterestRate: Double = 0.167
     val years: Int = 20
 
-    var multiplier: Double = 1.0
-    for (i in 1..years) {
-        multiplier *= (1 + annualInterestRate)
-    }
-    val finalAmount: Double = initialDeposit * multiplier
-
+    val finalAmount: Double = initialDeposit * (1 + annualInterestRate).pow(years)
     println(String.format("%.3f", finalAmount))
 }
