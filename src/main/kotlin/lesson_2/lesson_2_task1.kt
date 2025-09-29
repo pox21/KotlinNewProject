039f8151f47bdd4aa8af27c7fc8cd5409822c44b
Web-Entwickler <pixpox21@bk.ru>
2025-09-29 19:24:28 +0200

    KS-1-2 removed add const STUDENTS_COUNT Task 1

--- src/main/kotlin/lesson_2/lesson_2_task1.kt
+++ src/main/kotlin/lesson_2/lesson_2_task1.kt
@@ -1,12 +1,11 @@
 package lesson_2
-
+const val STUDENTS_COUNT = 4
 fun main() {
-
     val scoreFirstStudent: Int = 3
     val scoreSecondStudent: Int = 4
     val scoreThirdStudent: Int = 3
     val scoreFourthStudent: Int = 5
 
-    val averageScore: Double = (scoreFirstStudent + scoreSecondStudent + scoreThirdStudent + scoreFourthStudent) / 4.0
+    val averageScore: Double = (scoreFirstStudent + scoreSecondStudent + scoreThirdStudent + scoreFourthStudent).toDouble() / STUDENTS_COUNT
     println(averageScore)
 }
