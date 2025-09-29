d1b6a6a56864a22c02b4bcf1c2cc45e9b48269ff
Web-Entwickler <pixpox21@bk.ru>
2025-09-29 21:04:18 +0200

    KS-2-5 refactor Task 5

--- src/main/kotlin/lesson_2/lesson_2_task5.kt
+++ src/main/kotlin/lesson_2/lesson_2_task5.kt
@@ -1,12 +1,14 @@
 package lesson_2
 
 import kotlin.math.pow
+const val HUNDRED_PERCENT: Double = 100.0;
 
 fun main() {
-    val initialDeposit: Double = 70_000.0
-    val annualInterestRate: Double = 0.167
+    val initialDeposit: Int = 70000
+    val annualInterestRate: Double = 16.7
     val years: Int = 20
 
-    val finalAmount: Double = initialDeposit * (1 + annualInterestRate).pow(years)
+    val finalAmount: Double =
+        initialDeposit * (1 + annualInterestRate / HUNDRED_PERCENT).pow(years)
     println(String.format("%.3f", finalAmount))
 }
