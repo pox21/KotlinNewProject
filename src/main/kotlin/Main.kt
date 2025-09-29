87795e9eff30460ad3af28de41d3ad3b134ee162
Web-Entwickler <pixpox21@bk.ru>
2025-09-29 11:34:20 +0200

    KS-2-1 removed empty lines Task 2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,9 +3,6 @@ fun main(args: Array<String>) {
     val name = readln()
 
     println("$hello $name!")
-
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
     println("Program arguments: ${args.joinToString()}")
 }
 
